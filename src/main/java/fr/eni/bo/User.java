834101b886d01bcba63859090be9b98b10e1fb9b
cintiald <cintia.lopezdelao2022@campus-eni.fr>
2023-01-10 09:50:22 +0100

    UpdatedUser

--- src/main/java/fr/eni/bo/User.java
+++ src/main/java/fr/eni/bo/User.java
@@ -4,30 +4,29 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name ="table_user")
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long idUser;
-	private String firstName;
 	private String lastName;
-	private String phoneNumner;
+	private String firstName;
+	private String email;
+	private String phoneNumber;
+	private String password;
 	private String street;
-	private String city;
 	private String zipCode;
+	private String city;
+	private boolean admin = false;
 	public long getIdUser() {
 		return idUser;
 	}
 	public void setIdUser(long idUser) {
 		this.idUser = idUser;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
 	}
 	public String getLastName() {
 		return lastName;
@@ -35,11 +34,29 @@ public class User {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public String getPhoneNumner() {
-		return phoneNumner;
+	public String getFirstName() {
+		return firstName;
 	}
-	public void setPhoneNumner(String phoneNumner) {
-		this.phoneNumner = phoneNumner;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	public String getStreet() {
 		return street;
@@ -47,55 +64,74 @@ public class User {
 	public void setStreet(String street) {
 		this.street = street;
 	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
 	public String getZipCode() {
 		return zipCode;
 	}
 	public void setZipCode(String zipCode) {
 		this.zipCode = zipCode;
 	}
-	public User(long idUser, String firstName, String lastName, String phoneNumner, String street, String city,
-			String zipCode) {
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public boolean isAdmin() {
+		return admin;
+	}
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	public User(long idUser, String lastName, String firstName, String email, String phoneNumber, String password,
+			String street, String zipCode, String city, boolean admin) {
 		super();
 		this.idUser = idUser;
-		this.firstName = firstName;
 		this.lastName = lastName;
-		this.phoneNumner = phoneNumner;
+		this.firstName = firstName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.password = password;
 		this.street = street;
-		this.city = city;
 		this.zipCode = zipCode;
+		this.city = city;
+		this.admin = admin;
 	}
-	public User(String firstName, String lastName, String phoneNumner, String street, String city, String zipCode) {
+	public User(String lastName, String firstName, String email, String phoneNumber, String password, String street,
+			String zipCode, String city, boolean admin) {
 		super();
-		this.firstName = firstName;
 		this.lastName = lastName;
-		this.phoneNumner = phoneNumner;
+		this.firstName = firstName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.password = password;
 		this.street = street;
-		this.city = city;
 		this.zipCode = zipCode;
+		this.city = city;
+		this.admin = admin;
 	}
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("User [idUser=");
 		builder.append(idUser);
-		builder.append(", firstName=");
-		builder.append(firstName);
 		builder.append(", lastName=");
 		builder.append(lastName);
-		builder.append(", phoneNumner=");
-		builder.append(phoneNumner);
+		builder.append(", firstName=");
+		builder.append(firstName);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append(", phoneNumber=");
+		builder.append(phoneNumber);
+		builder.append(", password=");
+		builder.append(password);
 		builder.append(", street=");
 		builder.append(street);
-		builder.append(", city=");
-		builder.append(city);
 		builder.append(", zipCode=");
 		builder.append(zipCode);
+		builder.append(", city=");
+		builder.append(city);
+		builder.append(", admin=");
+		builder.append(admin);
 		builder.append("]");
 		return builder.toString();
 	}
--- src/main/java/fr/eni/controllerMmi/controller/ControllerConnection.java
+++ src/main/java/fr/eni/controllerMmi/controller/ControllerConnection.java
@@ -3,6 +3,7 @@ package fr.eni.controllerMmi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import fr.eni.bll.ServiceConnection;
@@ -22,7 +23,10 @@ public class ControllerConnection {
 		return "view-login";
 	}
 	
-	
+	@PostMapping("/login")
+	public String login(@RequestParam(required=true)String firstName, String lastName, String email, String telephone, ) {
+		
+	}
 	
 	
 	
