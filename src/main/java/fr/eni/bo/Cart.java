070839d4b42b762fa841918b62e9d2548125353f
skone2022 <skone2022@103SE41-5VV6YW2.ad.campus-eni.fr>
2023-01-10 09:57:44 +0100

    conflit cart

--- src/main/java/fr/eni/bo/Cart.java
+++ src/main/java/fr/eni/bo/Cart.java
@@ -1,70 +1,70 @@
-//package fr.eni.bo;
-//
-//import java.util.List;
-//
-//public class Cart {
-//
-//	private long cartId;
-//	private double total;
-//	List<Cure> lstCure;
-//	
-//	public Cart() {
-//		super();
-//	}
-//
-//	public Cart(double total, List<Cure> lstCure) {
-//		super();
-//		this.total = total;
-//		this.lstCure = lstCure;
-//	}
-//
-//	public Cart(long cartId, double total, List<Cure> lstCure) {
-//		super();
-//		this.cartId = cartId;
-//		this.total = total;
-//		this.lstCure = lstCure;
-//	}
-//
-//	public long getCartId() {
-//		return cartId;
-//	}
-//
-//	public void setCartId(long cartId) {
-//		this.cartId = cartId;
-//	}
-//
-//	public double getTotal() {
-//		return total;
-//	}
-//
-//	public void setTotal(double total) {
-//		this.total = total;
-//	}
-//
-//	public List<Cure> getLstCure() {
-//		return lstCure;
-//	}
-//
-//	public void setLstCure(List<Cure> lstCure) {
-//		this.lstCure = lstCure;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		StringBuilder builder = new StringBuilder();
-//		builder.append("Cart [cartId=");
-//		builder.append(cartId);
-//		builder.append(", total=");
-//		builder.append(total);
-//		builder.append("]");
-//		return builder.toString();
-//	}
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//}
+package fr.eni.bo;
+
+import java.util.List;
+
+public class Cart {
+
+	private long cartId;
+	private double total;
+	List<Cure> lstCure;
+	
+	public Cart() {
+		super();
+	}
+
+	public Cart(double total, List<Cure> lstCure) {
+		super();
+		this.total = total;
+		this.lstCure = lstCure;
+	}
+
+	public Cart(long cartId, double total, List<Cure> lstCure) {
+		super();
+		this.cartId = cartId;
+		this.total = total;
+		this.lstCure = lstCure;
+	}
+
+	public long getCartId() {
+		return cartId;
+	}
+
+	public void setCartId(long cartId) {
+		this.cartId = cartId;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public List<Cure> getLstCure() {
+		return lstCure;
+	}
+
+	public void setLstCure(List<Cure> lstCure) {
+		this.lstCure = lstCure;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Cart [cartId=");
+		builder.append(cartId);
+		builder.append(", total=");
+		builder.append(total);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
