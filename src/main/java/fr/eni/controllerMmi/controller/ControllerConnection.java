311629566c6b542038f3b4bfd352d8f6b7ce594b
cintiald <cintia.lopezdelao2022@campus-eni.fr>
2023-01-10 11:51:37 +0100

    Updated

--- src/main/java/fr/eni/controllerMmi/controller/ControllerConnection.java
+++ src/main/java/fr/eni/controllerMmi/controller/ControllerConnection.java
@@ -1,12 +1,14 @@
 package fr.eni.controllerMmi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import fr.eni.bll.ServiceConnection;
+import fr.eni.bo.User;
 
 @Controller
 @SessionAttributes({"userSession"})
@@ -23,13 +25,14 @@ public class ControllerConnection {
 		return "view-login";
 	}
 	
-	@PostMapping("/login")
-	public String login(@RequestParam(required=true)String firstName, String lastName, String email, String telephone, ) {
-		
+@PostMapping("/login")
+public String login(@RequestParam(required=true)String email, String password, Model model) {
+	String retour = "view-login";
+	User user = serviceConnection.login(email, password);
+	if ( user != null) {
+		model.addAttribute("userSession", user);
+		retour = "redirect:/cures";
 	}
-	
-	
-	
-	
-	
+	return retour;
+	}
 }
